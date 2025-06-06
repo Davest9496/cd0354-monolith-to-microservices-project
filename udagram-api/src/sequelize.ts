ad61c8cec2587e0d7762501d2d30cd2e3b13bd26
Dave Ejezie <evergreendave1@gmail.com>
2025-06-06 08:47:09 +0100

    initial server setup complete

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,19 +1,25 @@
-import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
+import { Sequelize } from "sequelize-typescript";
+import { Dialect } from "sequelize";
+import { config } from "./config/config";
 
+const isDevelopment =
+  process.env.NODE_ENV === "development" || !process.env.NODE_ENV;
+const isLocalhost = config.host === "localhost" || config.host === "127.0.0.1";
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
-
-  'dialect': config.dialect,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false 
-    }
-  },
-  'storage': ':memory:',
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  dialect: config.dialect as Dialect,
+  dialectOptions:
+    isDevelopment || isLocalhost
+      ? {}
+      : {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+  storage: ":memory:",
 });
